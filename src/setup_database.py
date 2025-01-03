68341bd2b0ac61c4c4250c2194870db7d296cb71
Koyo Nakayama <taiyou.003.084@gmail.com>
2025-01-03 18:32:06 +0900

    Add setup-database feature with tests

--- src/setup_database.py
+++ src/setup_database.py
@@ -1,6 +1,7 @@
 import os
+import time
 import pyalex
-from pyalex import Works, Authors
+from pyalex import Works, Authors, Institutions
 from neo4j import GraphDatabase
 from neo4j.exceptions import ServiceUnavailable
 
@@ -30,58 +31,48 @@ class Neo4jHandler:
         except Exception as e:
             raise RuntimeError(f"An error occurred while executing query: {e}")
 
-    def add_to_batch(self, query: str) -> None:
-        self.query_buffer.append(query)
+    def add_to_batch(self, query: str, parameters: dict = None) -> None:
+        self.query_buffer.append((query, parameters or {}))
 
     def flush(self):
         if not self.query_buffer:
             return
-        else:
-            self.execute_query(" ".join(self.query_buffer))
+        try:
+            with self.driver.session() as session:
+                session.execute_write(
+                    lambda tx: [tx.run(query, **params) for query, params in self.query_buffer]
+                )
             self.query_buffer.clear()
             self.id_histoty.clear()
-
-    # def add_work(self, work: Works) -> None:
-    #     self.execute_query(
-    #         "MERGE (n:Work {id: $id, title: $title})",
-    #         id=work["id"].replace("https://openalex.org/", ""),
-    #         title=work["title"]
-    #     )
+        except Exception as e:
+            raise RuntimeError(f"An error occurred while flushing queries: {e}")
 
     def add_work(self, work: Works) -> None:
         id = work["id"].replace("https://openalex.org/", "")
         if id not in self.id_histoty:
             self.add_to_batch(
-                "MERGE (" + id + ":Work {id: '" + id + "', title: '" + work["title"] + "'})"
+                "MERGE (n:Work {id: $id, title: $title})",
+                {"id": id, "title": work["title"]}
             )
             self.id_histoty.append(id)
-
-    # def add_author(self, author: Authors) -> None:
-    #     self.execute_query(
-    #         "MERGE (n:Author {id: $id, display_name: $display_name})",
-    #         id=author["id"].replace("https://openalex.org/", ""),
-    #         display_name=author["display_name"]
-    #     )
 
     def add_author(self, author: Authors) -> None:
         id = author["id"].replace("https://openalex.org/", "")
         if id not in self.id_histoty:
             self.add_to_batch(
-                "MERGE (" + id + ":Author {id: '" + id + "', display_name: '" + author["display_name"] + "'})"
+                "MERGE (n:Author {id: $id, display_name: $display_name})",
+                {"id": id, "display_name": author["display_name"]}
             )
             self.id_histoty.append(id)
-
-    # def add_referenced(self, work1: Works, work2: Works) -> None:
-    #     """
-    #     `work1` referenced `work2`
-    #     """
-    #     self.execute_query(
-    #         "MATCH (n1:Work {id: $id1}), (n2:Work {id: $id2})"
-    #         "MERGE (n1)-[r:REFERENCED]->(n2)"
-    #         "RETURN r",
-    #         id1=work1["id"].replace("https://openalex.org/", ""),
-    #         id2=work2["id"].replace("https://openalex.org/", "")
-    #     )
+    
+    def add_institution(self, institution: Institutions) -> None:
+        id = institution["id"].replace("https://openalex.org/", "")
+        if id not in self.id_histoty:
+            self.add_to_batch(
+                "MERGE (n:Institution {id: $id, display_name: $display_name})",
+                {"id": id, "display_name": institution["display_name"]}
+            )
+            self.id_histoty.append(id)
 
     def add_referenced(self, work1: Works, work2: Works) -> None:
         """
@@ -90,81 +81,103 @@ class Neo4jHandler:
         id1 = work1["id"].replace("https://openalex.org/", "")
         id2 = work2["id"].replace("https://openalex.org/", "")
         self.add_to_batch(
-            "MERGE (" + id1 + ")-[" + id1 + id2 + ":REFERENCED]->(" + id2 + ")"
+            "MATCH (n1:Work {id: $id1}), (n2:Work {id: $id2})"
+            "MERGE (n1)-[r:REFERENCED]->(n2)"
+            "RETURN r",
+            {"id1": id1, "id2": id2}
         )
-
-    # def add_authored(self, author: Authors, work: Works) -> None:
-    #     self.execute_query(
-    #         "MATCH (n1:Author {id: $id1}), (n2:Work {id: $id2})"
-    #         "MERGE (n1)-[r:AUTHORED]->(n2)"
-    #         "RETURN r",
-    #         id1=author["id"].replace("https://openalex.org/", ""),
-    #         id2=work["id"].replace("https://openalex.org/", "")
-    #     )
 
     def add_authored(self, author: Authors, work: Works) -> None:
         id1 = author["id"].replace("https://openalex.org/", "")
         id2 = work["id"].replace("https://openalex.org/", "")
         self.add_to_batch(
-            "MERGE (" + id1 + ")-[" + id1 + id2 + ":AUTHORED]->(" + id2 + ")"
+            "MATCH (n1:Author {id: $id1}), (n2:Work {id: $id2})"
+            "MERGE (n1)-[r:AUTHORED]->(n2)"
+            "RETURN r",
+            {"id1": id1, "id2": id2}
         )
 
-    # def add_works(self, initial_work: Works, depth: int = 3) -> None:
-    #     self.add_work(initial_work)
-    #     for authorship in initial_work["authorships"]:
-    #         author = Authors()[authorship["author"]["id"]]
-    #         self.add_author(author)
-    #         self.add_authored(author, initial_work)
-    #     if depth > 0:
-    #         for referenced_work in initial_work["referenced_works"]:
-    #             work = Works()[referenced_work]
-    #             self.add_works(work, depth - 1)
-    #             self.add_referenced(initial_work, work)
+    def add_affiliated_with(self, author: Authors, institution: Institutions) -> None:
+        id1 = author["id"].replace("https://openalex.org/", "")
+        id2 = institution["id"].replace("https://openalex.org/", "")
+        self.add_to_batch(
+            "MATCH (n1:Author {id: $id1}), (n2:Institution {id: $id2})"
+            "MERGE (n1)-[r:AFFILIATED_WITH]->(n2)"
+            "RETURN r",
+            {"id1": id1, "id2": id2}
+        )
 
-    def add_works(self, initial_work: Works, depth: int = 3) -> None:
+    def traverse_and_add_works(self, initial_work: Works, depth: int = 3) -> None:
         self.add_work(initial_work)
-        for authorship in initial_work["authorships"]:
-            author = Authors()[authorship["author"]["id"]]
+        author_ids = [authorship["author"]["id"].replace("https://openalex.org/", "") for authorship in initial_work["authorships"]]
+        authors = []
+        if len(author_ids) != 0:
+            for i in range(len(author_ids) // 100 + 1):
+                try:
+                    authors = authors + Authors().filter(openalex_id="|".join(author_ids[i*100:(i+1)*100])).get(per_page=100)
+                    time.sleep(0.1)
+                except Exception as e:
+                    print(f"Error fetching authors: {e}")
+        for author in authors:
             self.add_author(author)
             self.add_authored(author, initial_work)
+            institution_ids = [affiliation["institution"]["id"].replace("https://openalex.org/", "") for affiliation in author["affiliations"]]
+            institutions = []
+            if len(institution_ids) != 0:
+                for i in range(len(institution_ids) // 100 + 1):
+                    try:
+                        institutions = institutions + Institutions().filter(openalex_id="|".join(institution_ids[i*100:(i+1)*100])).get(per_page=100)
+                        time.sleep(0.1)
+                    except Exception as e:
+                        print(f"Error fetching institutions: {e}")
+            for institution in institutions:
+                self.add_institution(institution)
+                self.add_affiliated_with(author, institution)
         if depth > 0:
-            for referenced_work in initial_work["referenced_works"]:
-                work = Works()[referenced_work]
-                self.add_works(work, depth - 1)
+            referenced_work_ids = [referenced_work.replace("https://openalex.org/", "") for referenced_work in initial_work["referenced_works"]]
+            works = []
+            if len(referenced_work_ids) != 0:
+                for i in range(len(referenced_work_ids) // 100 + 1):
+                    try:
+                        works = works + Works().filter(openalex_id="|".join(referenced_work_ids[i*100:(i+1)*100])).get(per_page=100)
+                        time.sleep(0.1)
+                    except Exception as e:
+                        print(f"Error fetching works: {e}")
+            for work in works:
+                self.traverse_and_add_works(work, depth - 1)
                 self.add_referenced(initial_work, work)
 
-    def create_graph_by_seed_work(self, initial_work: Works, depth: int = 3) -> None:
-        self.query_buffer.clear()
-        self.add_works(initial_work, depth)
+    def build_graph_from_work(self, initial_work: Works, depth: int = 3) -> None:
+        self.traverse_and_add_works(initial_work, depth)
         self.flush()
 
 
-pyalex.config.email = os.getenv("OPENALEX_EMAIL")
+if __name__ == "__main__":
+    pyalex.config.email = os.getenv("OPENALEX_EMAIL")
 
-neo4j_handler = Neo4jHandler(
-    uri=os.getenv("NEO4J_URI"),
-    username=os.getenv("NEO4J_USERNAME"),
-    password=os.getenv("NEO4J_PASSWORD")
-)
+    neo4j_handler = Neo4jHandler(
+        uri=os.getenv("NEO4J_URI"),
+        username=os.getenv("NEO4J_USERNAME"),
+        password=os.getenv("NEO4J_PASSWORD")
+    )
 
-neo4j_handler.execute_query(
-    "MATCH (n)"
-    "DETACH DELETE n"
-)
+    # neo4j_handler.execute_query(
+    #     "MATCH (n)"
+    #     "DETACH DELETE n"
+    # )
 
-neo4j_handler.execute_query(
-    "CREATE TEXT INDEX node_text_index_id IF NOT EXISTS FOR (n:Work) ON (n.id)"
-)
+    neo4j_handler.execute_query(
+        "CREATE TEXT INDEX node_text_index_id IF NOT EXISTS FOR (n:Work) ON (n.id)"
+    )
 
-dois = [
-    "https://doi.org/10.1007/s11548-019-01929-x",
-    "https://dx.doi.org/10.3748/wjg.v29.i9.1427",
-    "https://doi.org/10.48550/arXiv.1706.03762"
-]
+    dois = [
+        "https://doi.org/10.1007/s11548-019-01929-x",
+        "https://dx.doi.org/10.3748/wjg.v29.i9.1427",
+        "https://doi.org/10.48550/arXiv.1706.03762"
+    ]
 
-for doi in dois:
-    work = Works()[doi]
-    # neo4j_handler.add_works(work, 0)
-    neo4j_handler.create_graph_by_seed_work(work, 1)
+    for doi in dois:
+        work = Works()[doi]
+        neo4j_handler.build_graph_from_work(work, 1)
 
-neo4j_handler.close()
+    neo4j_handler.close()
